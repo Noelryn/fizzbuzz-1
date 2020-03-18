88b39402552226d7c830759cf9b98385a295ca0c
allezz <aerbay@gmail.com>
2020-03-18 20:40:11 +0100

    Finish fizzbuzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,16 @@
-  def fizz_buzz(number)
-        if number % 3 == 0
-          'fizz'
+def fizz_buzz(number)
+    if (number.is_a? Integer) == false
+            p "fizz_buzz needs an Integer to run"
+        elsif number % 15 == 0
+            p "fizz buzz"
+        elsif number % 5 == 0
+            p "buzz"
+        elsif number % 3 == 0
+            p "fizz"
         else
-          number
-        end 
-  end
+            p number
+    end 
+end
+
+
+
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,6 +1,18 @@
 require "./lib/fizz_buzz"
 describe "fizz_buzz" do
-    it 'returns "fizz" if number is divisible by 3' do
-        expect(fizz_buzz).to eq "fizz" 
+    it "returns fizz if number is divisible by 3" do
+        expect(fizz_buzz(9)).to eq "fizz" 
+    end
+    it "returns buzz if number is divisible by 5 " do
+        expect(fizz_buzz(20)).to eq "buzz"
+    end
+    it "return fizz buzz if number is divisible by 15" do
+        expect(fizz_buzz(60)).to eq "fizz buzz"
+    end
+    it "returns a string if a number wasn't given" do
+        expect(fizz_buzz("lol")).to eq "fizz_buzz needs an Integer to run"
+    end
+    it "return number if a number is not divisible by 3,5,15" do
+        expect(fizz_buzz(1)).to eq 1
     end
 end
